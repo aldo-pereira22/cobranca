d30e9a485186519546d00c259d011da8df7e5c96
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-03-03 13:38:35 -0300

    gerar PDF

--- src/cobranca/boleto/acao/AcaoGeraBoleto.java
+++ src/cobranca/boleto/acao/AcaoGeraBoleto.java
@@ -1,8 +1,19 @@
 package cobranca.boleto.acao;
 
+import java.io.FileOutputStream;
 import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.Barcode;
+import com.itextpdf.text.pdf.Barcode128;
+import com.itextpdf.text.pdf.Barcode39;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import cobranca.entidade.Boleto;
 import cobranca.entidade.Contrato;
@@ -30,7 +41,68 @@ public class AcaoGeraBoleto {
 		boleto.setCodigo(Utilitario.geraCodigo());
 		boleto.setContrato(contrato);	
 		BoletoDAO bdao = new BoletoDAO();		
-		bdao.salvar(boleto);	
+		bdao.salvar(boleto);
+		
+		
+		Document documento = new Document(PageSize.A4, 50,50,50,50);
+		
+		
+		try {
+			
+			PdfWriter pdf = PdfWriter.getInstance(documento, new FileOutputStream(boleto.getContrato().getCliente().getNome()+"boleto.pdf"));
+			documento.open();
+			
+			Paragraph pnome = new Paragraph();
+			pnome.add("Nome do cliente: "+boleto.getContrato().getCliente().getNome());
+			
+			Paragraph pnumeroContrato = new Paragraph();
+			pnome.add("\nNumero do contrato : "+boleto.getContrato().getCodigo());
+			
+			Paragraph pservico = new Paragraph();
+			pnome.add("\nServico utilizado: "+boleto.getContrato().getServico().getNome());
+			
+			Paragraph pvalor = new Paragraph();
+			pnome.add("\nValor do servico "+boleto.getContrato().getValor());
+			
+			Paragraph p = new Paragraph();
+			pnome.add("\nCódigo de barras");
+			
+			Barcode128 code = new Barcode128();
+			code.setCode(boleto.getCodigo());
+			Image img = code.createImageWithBarcode(pdf.getDirectContent(), BaseColor.BLACK, BaseColor.BLACK);
+			
+			img.scalePercent(300);	
+			
+			
+			documento.add(pnome);
+			documento.add(pnumeroContrato);
+			documento.add(pservico);
+			documento.add(pvalor);
+			documento.add(p);
+			documento.add(img);
+			
+			
+			
+			documento.close();
+			
+			System.out.println("PDF GERADO");
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
 		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
